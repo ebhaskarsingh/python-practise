ca2034460059c1ec1bb8c0b309f9db1372abe426
bhaskarsingh <e.bhaskarsingh@gmail.com>
2026-03-28 08:03:48 -0700

    this is code of checking ehether the number is palinfrome or not

--- 2.py
+++ 2.py
@@ -9,3 +9,18 @@ def count_digit(num):
 n = int(input("enter the number: "))
 n = count_digit(n)
 print(n)
+
+# write a program to check whether the number is palindrome or not using recursion\\
+
+def palindrome(num, rev=0):
+    if num == 0:
+        return rev
+    return palindrome(num // 10, rev * 10 + num % 10)
+
+n = int(input("enter: "))
+reversed_num = palindrome(n)
+
+if n == reversed_num:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
