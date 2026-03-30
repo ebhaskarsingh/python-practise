9c2ba159afb9a9dcee85afb036a894d9b646c0f2
bhaskarsingh <e.bhaskarsingh@gmail.com>
2026-03-30 07:17:20 -0700

    this code find the permutatin of string

--- 5.py
+++ 5.py
@@ -18,3 +18,27 @@ def permutation(arr):
 
 
 permutation([1,2,3])
+
+
+
+## write a function to print all the permutation of a striing using backtracking\\
+
+def permutation_string(s):
+    char = list(s) # to convert the string into list\\
+
+    def solve(current):
+        if len(current) == len(char):
+            print(''.join(current)) # to conver the list into string and print it\\
+            return
+        
+        for i in char:
+            if i not in current:
+                current.append(i) # to add the item in current
+                solve(current) # backtracking
+
+                current.pop() # to remove the last item from current and try the next item in char\\
+
+
+    solve([])
+
+permutation_string("abc")
