6f26e76d396d828d7c09bf19223c81135b7c0507
bhaskarsingh <e.bhaskarsingh@gmail.com>
2026-03-31 07:33:01 -0700

    this code find the subset of an array whose sum is equal to 2

--- 6.py
+++ 6.py
@@ -1,29 +1,21 @@
-# write a function to print those set of an array whose sum is equeal to 2\
-
 def backtrack(arr,k):
     def solve(current, index, total):
         if index == len(arr):
             if total == k:
                 print(current)
-                return
+            return
             
 
-                 # include element\\
-
+        # include element
         current.append(arr[index])
         solve(current,index + 1, total + arr[index])
 
-           # exclude the element\
+        # backtrack
         current.pop()   
 
-
+        # exclude element
         solve(current,index + 1 , total)
-
-
 
     solve([],0,0)
 
-backtrack([1,2,1],2)
-
-
-
+backtrack([1,2,1],3)
