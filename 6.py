2a953c656ca60f008bc4b2b1a07d9ffca6aaf89c
bhaskarsingh <e.bhaskarsingh@gmail.com>
2026-03-31 21:35:03 -0700

    hello

--- 6.py
+++ 6.py
@@ -19,3 +19,35 @@ def backtrack(arr,k):
     solve([],0,0)
 
 backtrack([1,2,1],3)
+
+
+# write a function to print the subsequence of an array whose target is k and elements can be reused\\
+
+def backtrack(arr,target):
+    def solve(current, index, total):
+        if total == target:
+            print(current)
+            return
+        if index == len(arr) or total>target:
+            return
+        
+
+
+        # include element\\\
+        current.append(arr[index])
+        solve(current, index,total+arr[index])
+
+
+        # backtrack\\
+
+        current.pop()
+
+
+        # exclude element \\
+
+        solve(current, index+1, total)
+
+
+    solve([],0,0)
+
+backtrack([2,3,6,7],7)
